45548b4ff76d05a11265fed7326e9063f9efda33
Luke Edwards <luke.edwards05@gmail.com>
2017-08-02 10:50:13 -0700

    add base get method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function (input, opts) {
-	opts = opts || {};
-}
+'use strict';
+const https = require('https');
+
+const get = uri => new Promise((res, rej) => {
+	https.get(uri, r => {
+		let out = '';
+		const code = r.statusCode;
+		if (code >= 400) return rej();
+		if (code == 301 || code == 302) {
+			return get(r.headers.location).then(res);
+		}
+		r.on('data', str => out += str).on('end', _ => res(out));
+	}).on('error', rej);
+});
