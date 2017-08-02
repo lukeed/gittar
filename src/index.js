602901a6685031c1dba5bb2d1b51c8fbed949a49
Luke Edwards <luke.edwards05@gmail.com>
2017-08-02 11:12:30 -0700

    rewrite // code style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 'use strict';
 const https = require('https');
 
-const get = uri => new Promise((res, rej) => {
-	https.get(uri, r => {
-		let out = '';
-		const code = r.statusCode;
-		if (code >= 400) return rej();
-		if (code == 301 || code == 302) {
-			return get(r.headers.location).then(res);
-		}
-		r.on('data', str => out += str).on('end', _ => res(out));
-	}).on('error', rej);
-});
+function get(uri) {
+	let out = '';
+	return new Promise((res, rej) => {
+		https.get(uri, r => {
+			const code = r.statusCode;
+			if (code >= 400) return rej();
+			if (code > 300 && code < 400) return get(r.headers.location).then(res);
+			r.on('data', str => out += str).on('end', _ => res(out));
+		}).on('error', rej);
+	});
+}
+
